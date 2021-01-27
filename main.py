cdadee9a64f4758d0619ac2db2c85e3a22135ad6
Tymoteusz Perka <tymoperka@gmail.com>
2021-01-27 20:52:01 +0100

    LogisticRegression class added

--- main.py
+++ main.py
@@ -49,7 +49,59 @@ class map_class:
     Link_Class = []
 
 
+class LogisticRegression:
+    step = 0
+    n_of_inputs = 0
+    decision_boundary = 0.5
+    weights = []
+    def __init__(self, step, n_of_inputs):
+        self.step = step
+        self.n_of_inputs = n_of_inputs
+        for n in range(0, n_of_inputs):
+            self.weights.append(0.0)
+
+
+    def logisticFunction(self, path):
+        result = 0.0
+        for p, w in zip(path, self.weights):
+            result += p * w
+        return result
+
+
+    def predict(self, path):
+        return 1.0 / (1 + math.exp(-1 * self.logisticFunction(path)))
+
+
+    def calculateCost(self, prediction, classification):
+        return (1.0 - classification) * math.log(1.0 - prediction) - math.log(prediction) * classification
+
+
+    def updateWeights(self, path, classification, prediction):
+        for i in range(len(self.weights)):
+            self.weights[i] = self.weights[i] - self.step * path[i] * (prediction - classification)
+
+    def train(self, path, classification):
+        prediction = self.predict(path)
+        #cost = self.cost(prediction, classification)
+        self.updateWeights(path, prediction, classification)
+        self.step *= 0.999
+
+    def decide(self, path):
+        return self.predict(path) >= self.decision_boundary
+
 _no_of_input_layers=0
+
+
+def classify(dist):
+    dist /= 1000
+    if dist < 200.0:
+        return 3
+    elif dist >= 200 and dist < 400:
+        return 2
+    elif dist >= 400 and dist < 600:
+        return 1
+    else:
+        return 0
 
 
 
@@ -90,10 +142,13 @@ def import_data(map_class):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('Program Init')
-    city_map=map_class();
+    city_map=map_class()
     #Creating Neural Network
     import_data(city_map)
-
+    step = 3.0
+    parityBit = LogisticRegression(step=step, n_of_inputs= _no_of_input_layers)
+    lowerHalf = LogisticRegression(step=step, n_of_inputs=_no_of_input_layers)
+    upperHalf = LogisticRegression(step=step, n_of_inputs= _no_of_input_layers)
 
     net = nn.Sequential()
     # When instantiated, Sequential stores a chain of neural network layers.
